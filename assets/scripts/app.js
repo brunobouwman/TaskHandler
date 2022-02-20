0566b91ad755a43ac66548fb11bfc6d82e379a3a
Bruno Bouwman <brunobouwman@hotmail.com>
2022-02-20 00:17:58 +0000

    fixed some bugs, still have to optimize task activation/deactivation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -17,66 +17,107 @@ class Tasks {
   newTaskObj(title, description, extraInfo, id, action, taskEl) {
     const newTask = new TaskCreator(title, description, extraInfo, id);
     if (action === 'Finish') {
-      this.render(action, taskEl, newTask); //bind this?
+      this.render(action, false, taskEl, newTask);
     } else {
-      this.render(action, taskEl, newTask);
+      this.render(action, false, taskEl, newTask);
     }
   }
 
-  newTaskElement(action) {
-    const title = document.getElementById('title');
-    const description = document.getElementById('description');
-    const extraInfo = document.getElementById('extra-info');
-    const taskElement = document.createElement('li');
-    taskElement.id = this.id;
-    taskElement.classList = 'card';
-    taskElement.innerHTML = `
+  newTaskElement(action, alreadyExists, element, newTask) {
+    console.log(action, alreadyExists);
+    if (alreadyExists) {
+      element.querySelector('button:last-of-type').textContent = action;
+      this.render(action, alreadyExists, element, newTask);
+    }else {
+      const title = document.getElementById('title');
+      const description = document.getElementById('description');
+      const extraInfo = document.getElementById('extra-info');
+      const taskElement = document.createElement('li');
+      taskElement.id = this.id;
+      taskElement.classList = 'card';
+      taskElement.innerHTML = `
     <h2>${title.value}</h2>
     <p>${description.value}</p>
     <button class="alt">More Info</button>
     <button >${action}</button>`;
-    this.newTaskObj(
-      title.value,
-      description.value,
-      extraInfo.value,
-      this.id,
-      action,
-      taskElement
-    );
+      this.newTaskObj(
+        title.value,
+        description.value,
+        extraInfo.value,
+        this.id,
+        action,
+        taskElement
+      );
+    }
   }
 
-  render(action, taskEl, newTask) {
-    const handlers = new TaskHandler(); //pass action ?
+  render(action, alreadyExists, taskEl, newTask) {
+    const handlers = new TaskHandler();
     const onGoingTaskList = document.querySelector('ul');
     const finishedTaskList = document.getElementById('finished-list');
     handlers.removeBackAndModal();
-    if (action === 'Finish') {
-      handlers.onGoingTasks.push(newTask);
+    if (!alreadyExists) {
+      this.onGoingTasks.push(newTask);
       onGoingTaskList.append(taskEl);
-      const moreInfoBtn = taskEl.querySelector('button');
-      const activateBtn = taskEl.querySelector('button:last-of-type');
-      moreInfoBtn.addEventListener(
-        'click',
-        handlers.extraInfoHandler.bind(handlers, newTask)
-      );
-      activateBtn.addEventListener(
-        'click',
-        handlers.activateTaskHandler.bind(handlers, newTask, taskEl)
-      );
-    } else {
-      handlers.finishedTasks.push(newTask);
+        const moreInfoBtn = taskEl.querySelector('button');
+        const actionBtn = taskEl.querySelector('button:last-of-type');
+        moreInfoBtn.addEventListener(
+          'click',
+          handlers.extraInfoHandler.bind(handlers, newTask, this.onGoingTasks)
+        );
+        actionBtn.addEventListener(
+          'click',
+          handlers.actionTaskHandler.bind(
+            handlers,
+            newTask,
+            taskEl,
+            action,
+            this.onGoingTasks
+          )
+        );
+      } else if (alreadyExists && action === 'Activate') {
+        this.onGoingTasks.push(newTask);
+        onGoingTaskList.append(taskEl);
+        const moreInfoBtn = taskEl.querySelector('button');
+        const actionBtn = taskEl.querySelector('button:last-of-type');
+        moreInfoBtn.removeEventListener('click', handlers.extraInfoHandler);
+        actionBtn.removeEventListener('click', handlers.actionTaskHandler);
+        moreInfoBtn.addEventListener(
+          'click',
+          handlers.extraInfoHandler.bind(handlers, newTask, this.finishedTasks)
+        );
+        actionBtn.addEventListener(
+          'click',
+          handlers.actionTaskHandler.bind(
+            handlers,
+            newTask,
+            taskEl,
+            action,
+            this.finishedTasks
+          )
+        );
+      } else if (alreadyExists && action === 'Finish') {
+      this.finishedTasks.push(newTask);
       finishedTaskList.append(taskEl);
       const moreInfoBtn = taskEl.querySelector('button');
-      const finishBtn = taskEl.querySelector('button:last-of-type');
-      moreInfoBtn.addEventListener(
-        'click',
-        handlers.extraInfoHandler.bind(handlers, newTask)
-      );
-      finishBtn.addEventListener(
-        'click',
-        handlers.finishTaskHandler.bind(handlers, newTask, taskEl)
-      );
-    }
+        const actionBtn = taskEl.querySelector('button:last-of-type');
+        moreInfoBtn.removeEventListener('click', handlers.extraInfoHandler);
+        actionBtn.removeEventListener('click', handlers.actionTaskHandler);
+        moreInfoBtn.addEventListener(
+          'click',
+          handlers.extraInfoHandler.bind(handlers, newTask, this.onGoingTasks)
+        );
+        actionBtn.addEventListener(
+          'click',
+          handlers.actionTaskHandler.bind(
+            handlers,
+            newTask,
+            taskEl,
+            action,
+            this.onGoingTasks
+          )
+        );
+       }
     this.id++;
     handlers.clearInputs();
   }
@@ -84,8 +125,6 @@ class Tasks {
 
 class TaskHandler extends Tasks {
   id = 0;
-  onGoingTasks = [];
-  finishedTasks = [];
 
   constructor() {
     super();
@@ -127,73 +166,80 @@ class TaskHandler extends Tasks {
     this.showAddModal();
   }
 
-  extraInfoHandler(taskObj) {
+  extraInfoHandler(taskObj, taskArray) {
     const infoModal = document.getElementById('more-info-modal-id');
     this.showBackdrop();
     infoModal.classList.add('visible');
-    if(this.onGoingTasks.includes(taskObj)) {
-    for (const task of this.onGoingTasks) {
-      if (task.id === taskObj.id) {
-        infoModal.innerHTML = `
+    if (taskArray.includes(taskObj)) {
+      for (const task of taskArray) {
+        if (task.id === taskObj.id) {
+          infoModal.innerHTML = `
             <p>${task.extraInfo}</p>`;
-        break;
+          break;
+        }
       }
-    }
-  }else {
-    for (const task of this.finishedTasks) {
-      if (task.id === taskObj.id) {
-        infoModal.innerHTML = `
+    } else {
+      for (const task of taskArray) {
+        if (task.id === taskObj.id) {
+          infoModal.innerHTML = `
         <p>${task.extraInfo}</p>`;
-    break;
+          break;
+        }
       }
     }
   }
-}
 
-  finishTaskHandler(newTask, element) {
+  actionTaskHandler(newTask, element, action, taskArray) {
+    console.log(action);
     let taskIndex = 0;
-    const onGoingTaskSection = document.querySelector('ul');
+    const onGoingTaskList = document.querySelector('ul');
     const finishedTaskList = document.getElementById('finished-list');
-    for (const task of this.onGoingTasks) {
+    for (const task of taskArray) {
       if (task.id === newTask.id) {
         break;
       } else taskIndex++;
     }
-    this.onGoingTasks.splice(taskIndex, 1);
-    onGoingTaskSection.children[taskIndex].remove();
-    element.innerHTML = `<h2>${newTask.title}</h2>
-    <p>${newTask.description}</p>
-    <button class="alt">More Info</button>
-    <button >Activate</button>
-    `;
-    finishedTaskList.prepend(element);
-    const moreInfoBtn = element.querySelector('button');
-    const activateBtn = element.querySelector('button:last-of-type');
-    moreInfoBtn.addEventListener(
-      'click',
-      this.extraInfoHandler.bind(this, element.id)
-    );
-    activateBtn.addEventListener(
-      'click',
-      this.activateTaskHandler.bind(this, element)
-    );
+    if (action === 'Finish') {
+      if(onGoingTaskList.children.length !== 0){
+        onGoingTaskList.children[taskIndex].remove();
+        taskArray.splice(taskIndex, 1);
+      }
+        this.newTaskElement('Activate', true, element, newTask);
+    } else {
+      if(finishedTaskList.children.length !== 0) {
+      finishedTaskList.children[taskIndex].remove();
+      taskArray.splice(taskIndex, 1);
+      }
+      this.newTaskElement('Finish', true, element, newTask);
+    }
+
+    // const moreInfoBtn = element.querySelector('button');
+    // const activateBtn = element.querySelector('button:last-of-type');
+    // moreInfoBtn.addEventListener(
+    //   'click',
+    //   this.extraInfoHandler.bind(this, element.id)
+    // );
+    // activateBtn.addEventListener(
+    //   'click',
+    //   this.actionTaskHandler.bind(this, element)
+    // );
   }
 
-  activateTaskHandler(element, newTask) {
-    const onGoingTaskSection = document.querySelector('ul');
-    const finishedTaskList = document.getElementById('finished-list');
-    let taskIndex = 0;
-    for (const task of this.finishedTasks) {
-      //Try dictonaries
-      if (task.id === element.id) {
-        break;
-      } else taskIndex++;
-    }
-    const task = this.finishedTasks.splice(taskIndex, 1);
-    this.onGoingTasks.push(task);
-    finishedTaskList.children[taskIndex].remove();
-    onGoingTaskSection.append(element);
-  }
+  //   activateTaskHandler(element, newTask) {
+  //     const onGoingTaskList = document.querySelector('ul');
+  //     const finishedTaskList = document.getElementById('finished-list');
+  //     let taskIndex = 0;
+  //     for (const task of this.finishedTasks) {
+  //       //Try dictonaries
+  //       if (task.id === element.id) {
+  //         break;
+  //       } else taskIndex++;
+  //     }
+  //     const task = this.finishedTasks.splice(taskIndex, 1);
+  //     this.onGoingTasks.push(task);
+  //     finishedTaskList.children[taskIndex].remove();
+  //     onGoingTaskSection.append(element);
+  //   }
 }
 
 const App = new TaskHandler();
@@ -203,5 +249,5 @@ addTaskBtn.addEventListener('click', App.addTaskHandler.bind(App));
 const addTaskConfirm = document.getElementById('add-task-button-confirm');
 addTaskConfirm.addEventListener(
   'click',
-  Task.newTaskElement.bind(Task, 'Finish')
+  Task.newTaskElement.bind(Task, 'Finish', false)
 );
