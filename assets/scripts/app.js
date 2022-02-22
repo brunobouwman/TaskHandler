38f0566682e1a10874757ca807c245d82691a8ac
Bruno Bouwman <brunobouwman@hotmail.com>
2022-02-22 21:16:46 +0000

    drag and drop working, couldn't fix same list drop bug, tried if statements but somehow it won't work

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,6 +4,7 @@ class TaskCreator {
     this.description = description;
     this.extraInfo = extraInfo;
     this.id = id;
+    this.draggable = true;
   }
 }
 
@@ -16,16 +17,14 @@ class Tasks {
 
   newTaskObj(title, description, extraInfo, id, taskEl) {
     const newTask = new TaskCreator(title, description, extraInfo, id);
-    this.render('Finish', taskEl, newTask, id);
+    this.render('Finish', taskEl, newTask);
   }
 
-  newTaskElement(action, alreadyExists, element, newTask, updatedArray) {
-    if (action === 'Finish' && alreadyExists) {
-      this.onGoingTasks = [...updatedArray];
+  newTaskElement(action, element, newTask) {
+    if (action === 'Finish') {
       element.querySelector('button:last-of-type').textContent = 'Activate';
       this.render('Activate', element, newTask);
     } else {
-      this.finishedTasks = [...updatedArray];
       element.querySelector('button:last-of-type').textContent = 'Finish';
       this.render('Finish', element, newTask);
     }
@@ -36,7 +35,9 @@ class Tasks {
     const description = document.getElementById('description');
     const extraInfo = document.getElementById('extra-info');
     const taskElement = document.createElement('li');
+    this.addTaskEventListener(taskElement, this.id);
     taskElement.id = this.id;
+    taskElement.setAttribute('draggable', true);
     taskElement.classList = 'card';
     taskElement.innerHTML = `
     <h2>${title.value}</h2>
@@ -50,12 +51,21 @@ class Tasks {
       this.id,
       taskElement
     );
+    this.id++;
   }
 
+  /* <-- VERY IMPORTANT --> */
   clearEventListeners(element) {
     const clonedElement = element.cloneNode(true); //Makes a deep clone
     element.replaceWith(clonedElement);
     return clonedElement;
+  }
+
+  addTaskEventListener(element, id) {
+    element.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', id);
+      event.dataTransfer.effectAllowed = 'move';
+    });
   }
 
   render(action, taskEl, newTask) {
@@ -65,56 +75,34 @@ class Tasks {
     handlers.removeBackAndModal();
     const moreInfoBtn = taskEl.querySelector('button');
     let actionBtn = taskEl.querySelector('button:last-of-type');
-    actionBtn = this.clearEventListeners(actionBtn);
+    actionBtn = this.clearEventListeners(actionBtn, true); //makes a deep clone
+    // actionBtn = this.clearEventListeners(actionBtn);
     if (action === 'Finish') {
       this.onGoingTasks.push(newTask);
       onGoingTaskList.append(taskEl); //When you append an existing element, it automatically removes it from the previous location
-      taskEl.scrollIntoView();
+      taskEl.scrollIntoView({ behavior: 'smooth' }); //Can also use top and left as properties inside that object. ex {top: 50, left: 50, behavior: 'smooth'};
       moreInfoBtn.addEventListener(
         'click',
-        handlers.extraInfoHandler.bind(
-          handlers,
-          newTask,
-          this.onGoingTasks,
-          this.finishedTasks
-        )
+        handlers.extraInfoHandler.bind(this, newTask)
       );
       actionBtn.addEventListener(
         'click',
-        handlers.actionTaskHandler.bind(
-          handlers,
-          newTask,
-          taskEl,
-          action,
-          this.onGoingTasks
-        )
+        handlers.actionTaskHandler.bind(this, newTask, taskEl, action)
       );
     } else {
       this.finishedTasks.push(newTask);
       finishedTaskList.append(taskEl);
       // finishedTaskList.scrollTo(0, 5000);
-      taskEl.scrollIntoView();
+      taskEl.scrollIntoView({ behavior: 'smooth' }); //smooth transition
       moreInfoBtn.addEventListener(
         'click',
-        handlers.extraInfoHandler.bind(
-          handlers,
-          newTask,
-          this.onGoingTasks,
-          this.finishedTasks
-        )
+        handlers.extraInfoHandler.bind(this, newTask)
       );
       actionBtn.addEventListener(
         'click',
-        handlers.actionTaskHandler.bind(
-          handlers,
-          newTask,
-          taskEl,
-          action,
-          this.finishedTasks
-        )
+        handlers.actionTaskHandler.bind(this, newTask, taskEl, action)
       );
     }
-    this.id++;
     handlers.clearInputs();
   }
 }
@@ -162,19 +150,39 @@ class TaskHandler extends Tasks {
     this.showAddModal();
   }
 
-  extraInfoHandler(taskObj, onGoingTasks, finishedTasks) {
+  dragTaskHandler(list) {
+    list.addEventListener('dragenter', (event) => {
+      if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault();
+        list.parentElement.classList.add('droppable');
+      }
+    });
+    list.addEventListener('dragover', (event) => {
+      if (event.dataTransfer.types[0] === 'text/plain') {
+        event.preventDefault();
+      }
+    });
+
+    list.addEventListener('dragleave', (event) => {
+      if (event.relatedTarget.closest('ul') != list) {
+        list.parentElement.classList.remove('droppable');
+      }
+    });
+  }
+
+  extraInfoHandler(taskObj) {
     const infoModal = document.getElementById('more-info-modal-id');
     this.showBackdrop();
     infoModal.classList.add('visible');
-    if (onGoingTasks.includes(taskObj)) {
-      for (const task of onGoingTasks) {
+    if (this.onGoingTasks.includes(taskObj)) {
+      for (const task of this.onGoingTasks) {
         if (task.id === taskObj.id) {
           infoModal.innerHTML = `
             <p>${task.extraInfo}</p>`;
         }
       }
     } else {
-      for (const task of finishedTasks) {
+      for (const task of this.finishedTasks) {
         if (task.id === taskObj.id) {
           infoModal.innerHTML = `
         <p>${task.extraInfo}</p>`;
@@ -183,28 +191,17 @@ class TaskHandler extends Tasks {
     }
   }
 
-  actionTaskHandler(newTask, element, action, taskArray) {
-    const onGoingTaskList = document.querySelector('ul');
-    const finishedTaskList = document.getElementById('finished-list');
+  actionTaskHandler(newTask, element, action) {
     if (action === 'Finish') {
-      let updatedArray = taskArray.filter((p) => p.id !== newTask.id); // filters every element but the one we want to remove
-      this.newTaskElement(action, true, element, newTask, updatedArray);
-      // if (onGoingTaskList.children.length !== 0) {
-      // const el = document.getElementById(elId);
+      this.onGoingTasks = this.onGoingTasks.filter((p) => p.id !== newTask.id); // filters every element but the one we want to remove
+      this.newTaskElement(action, element, newTask);
       // const objIndex = taskArray.findIndex((p) => p.id === newTask.id);
-      // onGoingTaskList.children[objIndex].remove();
-      // this.onGoingTasks.splice(objIndex, 1);
-      // }
-      // else this.newTaskElement(action, true, element, newTask, onGoingTasks, finishedTasks);
     } else {
-      // if (finishedTaskList.children.length !== 0) {
-      // const objIndex = taskArray.findIndex((p) => p.id === newTask.id);
-      // finishedTaskList.children[objIndex].remove();
-      let updatedArray = taskArray.filter((p) => p.id !== newTask.id);
+      this.finishedTasks = this.finishedTasks.filter(
+        (p) => p.id !== newTask.id
+      );
       // finishedTasks.splice(objIndex, 1);
-      this.newTaskElement(action, true, element, newTask, updatedArray);
-      // }else this.newTaskElement(action, true, element, newTask, onGoingTasks, finishedTasks);
-      // }
+      this.newTaskElement(action, element, newTask);
     }
   }
 }
@@ -224,9 +221,33 @@ class TaskHandler extends Tasks {
 // }
 // App.init();
 
-const App = new TaskHandler();
+const Handlers = new TaskHandler();
 const Task = new Tasks();
 const addTaskBtn = document.getElementById('add-task-btn');
-addTaskBtn.addEventListener('click', App.addTaskHandler.bind(App));
+addTaskBtn.addEventListener('click', Handlers.addTaskHandler.bind(Handlers));
 const addTaskConfirm = document.getElementById('add-task-button-confirm');
 addTaskConfirm.addEventListener('click', Task.createTask.bind(Task, 'Finish'));
+const activeList = document.querySelector('ul');
+const finishedList = document.getElementById('finished-list');
+Handlers.dragTaskHandler(activeList);
+Handlers.dragTaskHandler(finishedList);
+
+activeList.addEventListener('drop', (event) => {
+  const projId = event.dataTransfer.getData('text/plain');
+  if (Task.onGoingTasks.find((p) => p.id === projId)) {
+    return;
+  }else 
+  document.getElementById(projId).querySelector('button:last-of-type').click();
+  activeList.parentElement.classList.remove('droppable');
+  event.preventDefault();
+});
+
+finishedList.addEventListener('drop', (event) => {
+  const projId = event.dataTransfer.getData('text/plain');
+  if (Task.finishedTasks.find((p) => p.id === projId)) {
+    return;
+  } else
+  document.getElementById(projId).querySelector('button:last-of-type').click();
+  finishedList.parentElement.classList.remove('droppable');
+  event.preventDefault();
+});
